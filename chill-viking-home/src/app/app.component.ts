df19d7c4ae23741e65981c15b49880125adcfd0f
Peter Jokumsen <pjokumsen@gmail.com>
2022-12-10 13:42:29 +0200

    feat: add chill-viking-home

---
+++ chill-viking-home/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chill-viking-home-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'chill-viking-home';
+}
